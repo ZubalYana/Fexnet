a9cf307bea1b6a793bf14bfe91ec0bfae7702852
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-29 19:01:39 +0300

    fix the mistakes and set the routers

---
+++ routes/fileroutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const multer = require('multer');
+const File = require('../models/fileModel');
+const code = require('../utils/generatecode');
+const router = express.Router();
+
+
+module.exports = router;
